36a80668fabe12cae24cb7b87688bab3b24d0f0a
dongsen <245307867@qq.com>
2016-06-06 11:15:56 +0800

    网络请求增加cookie 以及请求头的设置

--- TheProjectFrameWork/TheProjectFrameWork/Expand(扩展)/NetWork(网络)/NetWork/NetWork.h
+++ TheProjectFrameWork/TheProjectFrameWork/Expand(扩展)/NetWork(网络)/NetWork/NetWork.h
@@ -9,5 +9,25 @@
 #import <Foundation/Foundation.h>
 
 @interface NetWork : NSObject
-
+/** 成功回调 */
+typedef void(^SuccessBlock)(NSData * data);
+/** 失败回调 */
+typedef void(^ErrorBlock)(NSString * error);
+/**
+ *  Get请求
+ *
+ *  @param url        接口除根域名外地址
+ *  @param block      成功回调
+ *  @param errorblock 失败回调
+ */
++(void)GETNetWorkWithUrl:(NSString*)url successBlock:(SuccessBlock)block errorBlock:(ErrorBlock)errorblock;
+/**
+ *  Post请求
+ *
+ *  @param url        接口除根域名外地址
+ *  @param data       参数
+ *  @param block      成功回调
+ *  @param errorblock 失败回调
+ */
++(void)PostNetWorkWithUrl:(NSString*)url with:(id)data successBlock:(SuccessBlock)block errorBlock:(ErrorBlock)errorblock;
 @end
--- TheProjectFrameWork/TheProjectFrameWork/Expand(扩展)/NetWork(网络)/NetWork/NetWork.m
+++ TheProjectFrameWork/TheProjectFrameWork/Expand(扩展)/NetWork(网络)/NetWork/NetWork.m
@@ -7,7 +7,76 @@
 //
 
 #import "NetWork.h"
-
+#import "NetWorkType.h"
+#import "NetWork+BodyAndCookie.h"
 @implementation NetWork
+/**
+ *  get请求
+ *
+ *  @param url        请求Url
+ *  @param block      <#block description#>
+ *  @param errorblock <#errorblock description#>
+ */
++(void)GETNetWorkWithUrl:(NSString*)url successBlock:(SuccessBlock)block errorBlock:(ErrorBlock)errorblock
+{
+    if ([NetWorkType CheckNetWorkType]==NetWorkTypesNoNe) {
+        errorblock(@"没有网络");
+        return;
+    }
+    //接口拼接
+    NSString * getUrl = [NSString stringWithFormat:@"%@%@",KAppRootUrl,url];
+    //接口空字符去掉
+    NSString * getReplaceUrls = [getUrl stringByReplacingOccurrencesOfString:@" " withString:@""];
+    AFHTTPRequestOperationManager * manager = [AFHTTPRequestOperationManager manager];
+    manager.securityPolicy.allowInvalidCertificates = YES;
+    manager.requestSerializer = [AFHTTPRequestSerializer serializer];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    //设置请求体
+    [NetWork setHttpBody:manager.requestSerializer];
+    [manager.responseSerializer setAcceptableContentTypes: [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript",@"text/html",@"text/css", nil]];
+    [manager GET:getReplaceUrls parameters:nil success:^(AFHTTPRequestOperation * _Nonnull operation, id  _Nonnull responseObject) {
+        [NetWork getHttpCookie];
+        block(responseObject);
+
+    } failure:^(AFHTTPRequestOperation * _Nonnull operation, NSError * _Nonnull error) {
+        errorblock(error.description);
+    }];
+}
+/**
+ *  posy请求
+ *
+ *  @param url        请求url
+ *  @param data       参数
+ *  @param block      <#block description#>
+ *  @param errorblock <#errorblock description#>
+ */
++(void)PostNetWorkWithUrl:(NSString*)url with:(id)data successBlock:(SuccessBlock)block errorBlock:(ErrorBlock)errorblock
+{
+    if ([NetWorkType CheckNetWorkType]==NetWorkTypesNoNe) {
+        errorblock(@"没有网络");
+        return;
+    }
+    //接口拼接
+    NSString * postUrl = [NSString stringWithFormat:@"%@%@",KAppRootUrl,url];
+    //接口空字符去掉
+    NSString * postReplaceUrls = [postUrl stringByReplacingOccurrencesOfString:@" " withString:@""];
+    AFHTTPRequestOperationManager * manager = [AFHTTPRequestOperationManager manager];
+    manager.securityPolicy.allowInvalidCertificates = YES;
+    manager.requestSerializer = [AFHTTPRequestSerializer serializer];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    //设置请求体
+    [NetWork setHttpBody:manager.requestSerializer];
+    [manager.responseSerializer setAcceptableContentTypes: [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript",@"text/html",@"text/css", @"text/plain", nil]];
+    // 请求
+    [manager POST:postReplaceUrls parameters:data success:^(AFHTTPRequestOperation * _Nonnull operation, id  _Nonnull responseObject) {
+        // 获取的数据传回去
+        [NetWork getHttpCookie];
+        block(responseObject);
+    } failure:^(AFHTTPRequestOperation * _Nonnull operation, NSError * _Nonnull error) {
+        errorblock(error.description);
+    }];
+
+}
+
 
 @end
